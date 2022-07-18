0e14dc908cdec5fe71fbe828f59504bb9b0424e9
Tulio <tuliodutra27@gmail.com>
2022-07-18 20:36:10 -0300

    conectando ao servidor

--- imersao-java/src/App.java
+++ imersao-java/src/App.java
@@ -1,5 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.Builder;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // fazer a conexão HTTP, pegar os dados e salvar em uma string
+
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
+        URI endereco = URI.create(url);
+        
+        var client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
     }
 }
