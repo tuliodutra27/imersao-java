9ddd788b6b158510c383c33d92e71162e8a0a614
Tulio <tuliodutra27@gmail.com>
2022-07-19 11:18:40 -0300

    aula 1

--- imersao-java/src/App.java
+++ imersao-java/src/App.java
@@ -4,6 +4,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.Builder;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -17,6 +19,19 @@ public class App {
         HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+        //System.out.println(body);
+
+        // extrair alguns dados (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> lista_de_filmes = parser.parse(body);
+        //System.out.println(lista_de_filmes.get(0));
+
+        //exibir os dados 
+
+        for (Map<String,String> filme : lista_de_filmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
     }
 }
